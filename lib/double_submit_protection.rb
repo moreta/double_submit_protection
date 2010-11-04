c2c7f776e4ad1e37dc27a59656e88b4fafca0f86
DianthuDia <dianthudia@y7mail.com>
2010-11-04 17:48:23 +0900

    Use flash in place of session

--- lib/double_submit_protection.rb
+++ lib/double_submit_protection.rb
@@ -1,21 +1,19 @@
 module Hervalicious
   module DoubleSubmitProtection
 
-    TOKEN_FIELD_NAME = "submit_token"
-    SESSION_TOKEN_KEY = "submit_token"
+    TOKEN_NAME = "submit_token"
 
     module View
       def double_submit_token
-        session[SESSION_TOKEN_KEY] = Digest::MD5.hexdigest(rand.to_s)
-        hidden_field_tag(TOKEN_FIELD_NAME, session[SESSION_TOKEN_KEY])
+        flash[TOKEN_NAME] = Digest::MD5.hexdigest(rand.to_s)
+        hidden_field_tag(TOKEN_NAME, flash[TOKEN_NAME])
       end
     end
 
     module Controller
       def double_submit?
-        session_token = session[SESSION_TOKEN_KEY]
-        session[SESSION_TOKEN_KEY] = nil
-        session_token.nil? || ( (request.post? || request.put?) && (session_token != params[TOKEN_FIELD_NAME]) )
+        token = flash[TOKEN_NAME]
+        token.nil? || ( (request.post? || request.put?) && (token != params[TOKEN_NAME]) )
       end
     end
   end
