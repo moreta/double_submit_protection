24ee6ac7b6f6d3147b34653ef27b29b6a49c46f1
DianthuDia <dianthudia@y7mail.com>
2010-11-05 14:38:31 +0900

    Support multiple token

--- lib/double_submit_protection.rb
+++ lib/double_submit_protection.rb
@@ -1,19 +1,21 @@
 module Hervalicious
   module DoubleSubmitProtection
 
-    TOKEN_NAME = "submit_token"
+    DEFAULT_TOKEN_NAME = 'submit_token'
 
     module View
-      def double_submit_token
-        flash[TOKEN_NAME] = Digest::MD5.hexdigest(rand.to_s)
-        hidden_field_tag(TOKEN_NAME, flash[TOKEN_NAME])
+      def double_submit_token(token_name=nil)
+        token_name ||= DEFAULT_TOKEN_NAME
+        flash[token_name] = Digest::MD5.hexdigest(rand.to_s)
+        hidden_field_tag(token_name, flash[token_name])
       end
     end
 
     module Controller
-      def double_submit?
-        token = flash[TOKEN_NAME]
-        token.nil? || ( (request.post? || request.put?) && (token != params[TOKEN_NAME]) )
+      def double_submit?(token_name=nil)
+        token_name ||= DEFAULT_TOKEN_NAME
+        token = flash[token_name]
+        token.nil? || ( (request.post? || request.put?) && (token != params[token_name]) )
       end
     end
   end
